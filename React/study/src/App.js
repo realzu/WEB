92b3836202efe0fd5efa10aaeb2b9ac5a411cd78
진현주 <97022695+realzu@users.noreply.github.com>
2022-07-26 23:59:13 +0900

    react basic2

--- React/study/src/App.js
+++ React/study/src/App.js
@@ -11,13 +11,10 @@ function App() {
   // 자료 잠깐 저장할 때 state : import {useState} -> useState(보관할자료) -> let[작명(state에보관한자료), 작명(state변경도와주는함수)]
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']); //배열 가능
 
-  // cf) Destructuring 문법
-  // let num = [1, 2]; //array안의 값들을 빼줌
-  // let [a, c] = [1, 2];
-
   // state 쓰는 이유: 일반변수는 갑자기 변경되면 html에 자동으로 반영안되지만, state쓰던 html은 자동 재렌더링! (=페이지 안에서 자주 바뀌는 것들은 state)
 
   let [하트, 하트변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   // function 함수(){
   //   console.log(1);
@@ -36,9 +33,6 @@ function App() {
           copy[0] = '여자코트 추천';
           글제목변경(copy);
           }}>글수정
-          {/* state변경함수 특징 : 기존과 신규 비교해서 같으면(==) 변경안함 */}
-          {/* let arr = [1,2,3]; array/object 특징 : 변수에 '화살표'(어딨는지)만 저장된것 -> =을 통해 넣어봤자 array만수정했지 화살표는 수정안됐으니 -> '글제목'을 가르키는건 같아서 기존/신규를 같다고생각함 */}
-          {/* 즉, state가 array/object면 독립적 카피본을 만들어서 수정해야(shallow copy) */}
         </button>
 
         <button onClick={() => {
@@ -49,9 +43,6 @@ function App() {
         <div className='list'>
           <h4>{ 글제목[0] } <span onClick={() => { 하트변경(하트+1) }}>💗</span> {하트} </h4>{/* onClick은 함수(함수명or함수문법) 넣어야 */}
           {/* state 변경법: state변경함수(새로운state)   //등호x */}
-          {/* 함수 생성 예시 */}
-          {/* onClick={function(){ console.log(1)}} */}
-          {/* onClick={ () => { console.log(1)}} */} {/* () => {} */}
           <p>7월 25일 발행</p>
         </div>
         <div className='list'>
@@ -59,49 +50,34 @@ function App() {
           <p>7월 25일 발행</p>
         </div>
         <div className='list'>
-          <h4>{ 글제목[2] }</h4>
+          <h4 onClick={() => {
+            if (modal == true) {
+              setModal(false)
+            }else{
+              setModal(true)
+            }
+          }}>{ 글제목[2] }</h4>
           <p>7월 25일 발행</p>
         </div>
 
-        {/* 함수명 호출 둘다 가능 */}
-        {/* <Modal></Modal> */}
-        <Modal/>
+        { // html 작성 공간이라 if..(x) -> 삼항연산자          
+          modal == true ? <Modal/> : null   //null은 비어있는 html용으로 자주 사용
+        }        
+
     </div>
   );
 }
 
-// * 컴포넌트 만드는 법
-// 1. function 만들고 (위의 function바깥에)
-// 2. return () 안에 html 담기
-// 3. <함수명></함수명> 쓰기
 function Modal(){ //Modal 쓴게 컴포넌트 (대문자로 시작)
   return (
-    // <>
       <div className='modal'>
         <h4>제목</h4>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
-    //   <div></div>
-    // </>
   )
-}
-// * return 안에 html 병렬 기입하려면
+} // * return 안에 html 병렬 기입하려면
 // 큰태그로 감싸줌 - 의미없는 <div></div>대신 <></>사용가능. 이걸로 묶어줄수있
 
-// * 어떤걸 컴포넌트로 만드면 좋을까
-// 1. 반복적인 html 축약할 때
-// 2. 큰 페이지들
-// 3. 자주 변경되는것들
-
-// * 컴포넌트 단점
-// state 가져다쓸때 문제 : A함수의 변수는 B함수에서 맘대로 사용불가
-
-// 컴포넌트 만드는 문법2
-const Modal1 = () => {
-  return (
-    <div></div>
-  )
-}
 
 export default App;
---
+++ React/study/src/note.js
@@ -0,0 +1,68 @@
+/* eslint-disable */
+function note() {
+    /*
+    cf) Destructuring 문법
+    let num = [1, 2]; //array안의 값들을 빼줌
+    let [a, c] = [1, 2];
+    */
+
+
+    /* state
+    state 변경함수 특징 : 기존과 신규 비교해서 같으면(==) 변경안함 
+    let arr = [1,2,3]; array/object 특징 : 변수에 '화살표'(어딨는지)만 저장된것 -> =을 통해 넣어봤자 array만수정했지 화살표는 수정안됐으니 -> '글제목'을 가르키는건 같아서 기존/신규를 같다고생각함
+    즉, state가 array/object면 독립적 카피본을 만들어서 수정해야(shallow copy)
+
+    함수 생성 예시
+        onClick={function(){ console.log(1)}}
+        onClick={ () => { console.log(1)}}      // () => {}
+    */
+
+
+    /*
+    * 컴포넌트 만드는 법
+    1. function 만들고 (위의 function바깥에)
+    2. return () 안에 html 담기
+    3. <함수명></함수명> 쓰기
+
+    * 어떤걸 컴포넌트로 만드면 좋을까
+    1. 반복적인 html 축약할 때
+    2. 큰 페이지들
+    3. 자주 변경되는것들
+
+    * 컴포넌트 단점
+    state 가져다쓸때 문제 : A함수의 변수는 B함수에서 맘대로 사용불가
+    */
+
+    // * 컴포넌트 만드는 문법2
+    const Modal = () => {
+        return (
+            <div></div>
+        )
+    }
+
+    //  함수명 호출 방법2개 <Modal></Modal> 또는 <Modal/>
+
+
+    /* 7) 동적인 UI 만드는 step    -- Modal
+    1. html css로 미리 디자인 완성
+    2. UI의 현재 상태를 state로 저장
+    3. state에 따라 UI가 어떻게 보일지 작성
+    */
+
+
+    /* 8) map
+    1. array 자료 갯수만큼 함수안의 코드 실행해줌
+    2. 함수의 파라미터는 array 안에 있던 자료
+    3. return안의 값은 array로 담아줌
+    */
+    [1,2,3].map(function(a){
+        // console.log(a) //1 - 2 - 3
+        return '1235'
+    }) //map은 배열뒤에. map(콜백함수)
+
+
+
+
+}
+
+export default note;
