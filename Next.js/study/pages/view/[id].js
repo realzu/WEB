7dc5e05a3749a405f5bd60f6e7dcf33b3d288184
진현주 <97022695+realzu@users.noreply.github.com>
2022-11-07 17:35:13 +0900

    next.js : feat getServerSideProps

--- Next.js/study/pages/view/[id].js
+++ Next.js/study/pages/view/[id].js
@@ -2,28 +2,36 @@ import Axios from 'axios'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import Item from '../../src/component/item';
+import { Loader } from 'semantic-ui-react';
+import Head from 'next/head';
 
-const Post = () => {
-    const router = useRouter();
-    const { id } = router.query;
-
-    const [item, setItem] = useState({});
-
-    const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-
-    function getData() {
-        Axios.get(API_URL).then((res) => {
-            setItem(res.data);
-        });
-    }
-
-    useEffect(() => {
-        if (id && id > 0) {
-            getData();
-        }
-    }, [id]);
-
-    return <Item item={item} />
+const Post = ({item}) => { // getServerSideProps에서 넘어온 item
+    return (
+        <>
+            {item && (
+                <>
+                    <Head>
+                        <title>{item.name}</title>
+                        <meta name='description' content={item.description}></meta> {/* 검색엔진에 내용이 담기므로 SEO 최적화 굿 */}
+                    </Head>
+                    <Item item={item} />
+                </>
+            )}
+        </>
+    )
 }
 
 export default Post
+
+export async function getServerSideProps(context) {
+    const id = context.params.id; // 넘어온 상품아이디
+    const apiUrl = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+    const res = await Axios.get(apiUrl);
+    const data = res.data;
+
+    return {
+        props: {
+            item: data, // data를 item에 넣어줌
+        },
+    };
+;}
