3c2d4b61265c5c26df2c97be6511885737544f28
진현주 <97022695+realzu@users.noreply.github.com>
2022-11-07 17:36:27 +0900

    next.js: add loading icon, update router

--- Next.js/study/pages/about.js
+++ Next.js/study/pages/about.js
@@ -1,13 +1,34 @@
-import NavBar from "../components/NavBar";
-import Head from "next/head";
-import Seo from "../components/Seo";
+import { Divider, Header, List } from "semantic-ui-react";
 
 export default function About() {  //export default function는 필수
     return (
         <div>
-            {/* <NavBar /> */}
-            <Seo title="About" />
-            <h1>About 페이지 (o゜▽゜)o☆</h1>
+            <Header as='h3' style={{ paddingTop: 40 }} color='teal'>
+                회사 소개
+            </Header>
+            <Divider />
+            <List>
+                <List.Item>
+                    <List.Icon name="users"/>
+                    <List.Content>semantic UI</List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Icon name="marker"/>
+                    <List.Content>New York, NY</List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Icon name="mail"/>
+                    <List.Content>
+                        <a href="mailto:jinhh501@naver.com">jinhh501@naver.com</a>
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Icon name="linkify"/>
+                    <List.Content>
+                        <a href="http:///www.semantic-ui.com">semantic-ui.com</a>
+                    </List.Content>
+                </List.Item>
+            </List>
         </div>
     ) 
 } //react.js 컴포넌트를 export하는 파일을 pages에 두는것 -> next.js가 파일명 가져다 url이름으로 씀
--- Next.js/study/pages/index.js
+++ Next.js/study/pages/index.js
@@ -1,18 +1,20 @@
 import Axios from 'axios';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
-import { Divider, Header } from 'semantic-ui-react';
+import { Divider, Header, Loader } from 'semantic-ui-react';
 import ItemList from '../src/component/ItemList';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
     const [list, setList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); // 데이터 로딩 표시
 
     const API_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'; // $ npm i axios
 
     function getData() {
         Axios.get(API_URL).then(res => {
             setList(res.data);
+            setIsLoading(false);
         });
     }
 
@@ -24,17 +26,29 @@ export default function Home() {
         <div>
             <Head>
                 <title>HOME | 코딩앙마</title> {/* 타이틀 */}
+                <meta name='description' content='코딩 앙마 홈입니다.'></meta>
             </Head>
-            <Header as='h3' style={{ paddingTop: 40 }}>
-                베스트 상품
-            </Header>
-            <Divider />
-            <ItemList list={list.slice(0, 9)} /> {/* ★slice로 배열나누기 */}
-            <Header as='h3' style={{ paddingTop: 40 }}>
-                신상품
-            </Header>
-            <Divider />
-            <ItemList list={list.slice(9)} />
+            {isLoading && (
+                <div style={{ padding: '300px 0' }}>
+                    <Loader inline='centered' active>
+                        Loading
+                    </Loader>
+                </div>
+            )}
+            {!isLoading && (
+                <>
+                    <Header as='h3' style={{ paddingTop: 40 }}>
+                        베스트 상품
+                    </Header>
+                    <Divider />
+                    <ItemList list={list.slice(0, 9)} /> {/* ★slice로 배열나누기 */}
+                    <Header as='h3' style={{ paddingTop: 40 }}>
+                        신상품
+                    </Header>
+                    <Divider />
+                    <ItemList list={list.slice(9)} />
+                </>
+            )}
         </div>
     )
 }
