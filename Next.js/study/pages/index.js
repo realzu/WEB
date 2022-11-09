6b581bea52807512140e0610d0058895f14316da
진현주 <97022695+realzu@users.noreply.github.com>
2022-11-09 18:04:25 +0900

    next.js : getStaticProps

--- Next.js/study/pages/index.js
+++ Next.js/study/pages/index.js
@@ -52,3 +52,41 @@ export default function Home() {
         </div>
     )
 }
+
+
+// 정적 생성
+export function StaticHome() { // html -> 데이터 바로 생기니까 로딩표시도 필요x
+    return (
+        <div>
+            <Head>
+                <title>HOME | 코딩앙마</title>
+                <meta name='description' content='코딩 앙마 홈입니다.'></meta>
+            </Head>
+            <>
+                <Header as='h3' style={{ paddingTop: 40 }}>
+                    베스트 상품
+                </Header>
+                <Divider />
+                <ItemList list={list.slice(0, 9)} />
+                <Header as='h3' style={{ paddingTop: 40 }}>
+                    신상품
+                </Header>
+                <Divider />
+                <ItemList list={list.slice(9)} />
+            </>
+        </div>
+    )
+}
+
+export async function getStaticProps(context) {
+    const apiUrl = process.env.apiUrl; // apiUrl 추가해줬음
+    const res = await Axios.get(apiUrl);
+    const data = res.data;
+
+    return {
+        props: {
+            list: data,
+            name: process.env.name
+        }
+    };
+} // 위의 list에 data 넣어줌
