e63d2e47a30e0fa96fa000fafaf63db7a4845356
진현주 <97022695+realzu@users.noreply.github.com>
2022-11-12 23:21:02 +0900

    next.js : isFallback, getStaticPaths

--- Next.js/study/pages/detail/[id].js
+++ Next.js/study/pages/detail/[id].js
@@ -7,6 +7,18 @@ import Head from 'next/head';
 
 // 기존의 view/[id].js를 정적페이지로 만들기
 const Post = ({item, name}) => {
+    const router = useRouter();
+
+    if (router.isFallback) { // isFallback통해서 동기처리 (처음엔 true -> false)
+        return (
+            <div style={{ padding: '100px 0' }}>
+                <Loader active inline='centered'>
+                    Loading
+                </Loader>
+            </div>
+        )
+    }
+
     return (
         <>
             {item && (
@@ -26,13 +38,24 @@ export default Post
 
 // 프로덕션 모드로 실행 : 속도 빠름
 export async function getStaticPaths() {
+    const apiUrl = process.env.apiUrl;
+    const res = await Axios.get(apiUrl);
+    const data = res.data;
+
     return {
-        paths: [
-            { params: { id: '740' } },
-            { params: { id: '730' } },
-            { params: { id: '729' } },
-        ],
-        fallback: false
+        // paths: [
+        //     { params: { id: '740' } },
+        //     { params: { id: '730' } },
+        //     { params: { id: '729' } },
+        // ],
+
+        // 리스트 결과 기반으로 staticPath 넘겨줌
+        paths: data.slice(0, 9).map(item => ({
+            params: {
+                id: item.id.toString(),
+            }
+        })),
+        fallback: true
     };
 }
 // fallback이 false면 없는페이지 대응 안해줌
