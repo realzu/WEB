db50d88b612bef6601dcf1fe3b4c023d83d28132
진현주 <97022695+realzu@users.noreply.github.com>
2022-11-30 23:51:14 +0900

    React - jest) check watch mode

--- React/study/src/App.js
+++ React/study/src/App.js
@@ -144,17 +144,23 @@
 
 // - jest
 
-import logo from './logo.svg'
+import logo from './logo.svg';
+import './App2.css';
 
 function App() {
   return (
-    <div>App
-      <header>
-        <img src={logo} alt='logo'/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
--- React/study/src/App.test.js
+++ React/study/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />); // render: JSX(=> App)에 관한 가상 DOM 생성
-  const linkElement = screen.getByText(/learn react/i); // getByText: 표시되는 모든텍스트 기반으로 DOM에서 요소찾음 (인자는 정규식 or 'Lear React'(App.js에 있음))
+  // const linkElement = screen.getByText(/Learn react/i); // getByText: 표시되는 모든텍스트 기반으로 DOM에서 요소찾음 (인자는 정규식 or 'Lear React'(App.js에 있음))
+  const linkElement = screen.getByText('Learn react');
   expect(linkElement).toBeInTheDocument();
 });
